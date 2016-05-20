8bd8bc55ac7369d0d0e97b4807b5110163c02a5e
douzihao <290579323@qq.com>
2016-05-20 08:44:25 +0800

    test commit

--- flask/test.py
+++ flask/test.py
@@ -2,8 +2,11 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 @app.route('/user/<name>')
+@app.route('/sytest/<name>')
 def user(name):
     return '<h1>Hello,%s!</h1>' %name
+def sytest(name):
+    return '<h1>hello,%s!</h1>' %name
 def index():
     return '<h1> hello world!</h1>'
 if __name__=='__main__':
