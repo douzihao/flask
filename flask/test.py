41830d110291548c1df79a5bf8a192607ace750d
douzihao <290579323@qq.com>
2016-05-20 09:54:26 +0800

    ss

--- flask/test.py
+++ flask/test.py
@@ -1,13 +1,19 @@
-from flask import Flask
+from flask import Flask,redirect,abort
 app = Flask(__name__)
 @app.route('/')
-@app.route('/user/<name>')
-@app.route('/sytest/<name>')
-def user(name):
-    return '<h1>Hello,%s!</h1>' %name
-def sytest(name):
-    return '<h1>hello,%s!</h1>' %name
 def index():
     return '<h1> hello world!</h1>'
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello,%s!</h1>' %name
+@app.route('/red')
+def red():
+    return redirect("http://www.baidu.com")        
+@app.route('/get_user/<id>')
+def get_user(id):
+    user = load_user(id)
+    if not user:
+        abort(404)
+    return ('<h1>hello</h1>')
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True,host='192.168.8.140')
