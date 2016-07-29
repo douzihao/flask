50651aa7525f605acfcfd555bb12d5db3bab6818
douzihao <290579323@qq.com>
2016-07-29 09:30:05 +0800

    test

--- flask/ss.py
+++ flask/ss.py
@@ -1,1 +1,7 @@
-fjwiefjwioe
+from flask.ext.script import  Manager
+from flask import Flask
+app = Flask(__name__)
+manage = Manager(app)
+
+if __name__ == '__main__':
+    manage.run()
--- flask/test.py
+++ flask/test.py
@@ -1,19 +1,52 @@
 from flask import Flask,redirect,abort
+from flask import request
+from flask import make_response
+from flask import render_template
+from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+from flask.ext.moment import Moment
+from datetime import datetime
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+
+
 @app.route('/')
 def index():
-    return '<h1> hello world!</h1>'
+    user_agent= request.headers.get('User-Agent')
+    print request.headers.get("host")
+    response = make_response('<p> your browser is sss </p>')
+    response.set_cookie("ss",'33')
+    return response
+    #return '<p> your browser is %s </p>' %user_agent
+    #return '<h1> hello world!</h1>'
+
+@app.route('/testtem/<value>')
+def testtem(value):
+    return render_template("page.html")
+
+@app.route('/ss/<kw>')
+def ss(kw):
+    return redirect("http://www.baidu.com")
+
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello,%s!</h1>' %name
+
 @app.route('/red')
 def red():
-    return redirect("http://www.baidu.com")        
+    return redirect("http://www.baidu.com")
+
 @app.route('/get_user/<id>')
 def get_user(id):
-    user = load_user(id)
+    user = None
     if not user:
-        abort(404)
+        abort(500)
     return ('<h1>hello</h1>')
 if __name__=='__main__':
-    app.run(debug=True,host='192.168.8.140')
+    manager.run()
+    #app.run(debug=True,host='192.168.15.199')
