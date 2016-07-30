610cb8376813fcdc227d0e5b6975495a3d5e41d3
douzihao <290579323@qq.com>
2016-07-30 09:03:53 +0800

    test

--- flask/test.py
+++ flask/test.py
@@ -6,10 +6,24 @@ from flask.ext.script import Manager
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 from datetime import datetime
+from flask.ext.wtf import Form
+from wtforms import StringField,SubmitField,TextField,validators
+from wtforms.validators import DataRequired
+
+
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
+class NameForm(Form):
+    name = StringField('your name',[validators.DataRequired()])
+    #user = TabError("ss",[validators.length(min=4,max=20),validators.DataRequired])
+    submit = SubmitField('submit')
+
+class MyForm(Form):
+    name = StringField('name', validators=[DataRequired()])
+
 app = Flask(__name__)
+app.config["SECRET_KEY"]='test'
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -37,9 +51,14 @@ def ss(kw):
     return redirect("http://www.baidu.com")
 
 
-@app.route('/user/<name>')
+@app.route('/user/<name>',methods=['GET','POST'])
 def user(name):
-    return '<h1>Hello,%s!</h1>' %name
+    name = None
+    nameForm = NameForm()
+    if nameForm.validate_on_submit():
+        name = nameForm.name.data
+        nameForm.name.data = ''
+    return render_template("index.html",form=nameForm,name=name)
 
 
 @app.route('/red')
@@ -53,6 +72,14 @@ def get_user(id):
     if not user:
         abort(500)
     return ('<h1>hello</h1>')
+
+@app.route('/submit', methods=('GET', 'POST'))
+def submit():
+    form = MyForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('test.html', form=form)
+
 if __name__=='__main__':
     manager.run()
     #app.run(debug=True,host='192.168.15.199')
